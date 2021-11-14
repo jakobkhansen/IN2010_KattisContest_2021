8513c0d354ecaffb6f1d1bf97bc7711ec2666640
Sergey Jakobsen <46766403+sergiosja@users.noreply.github.com>
2021-11-14 20:54:01 +0100

    Create Twosum.java

---
+++ Java/Twosum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Twosum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Printer summen av to heltall fra terminalen
+        System.out.println(sc.nextInt() + sc.nextInt());
+
+        sc.close();
+    }
+}
