0f276a90b2e04a43f003cac1a24e37bc60604e2c
Jakob Hansen <jakob.hansen@hotmail.no>
2021-11-14 20:26:34 +0100

    Add two_sum python solution

---
+++ python/two_sum.py
@@ -0,0 +1,9 @@
+# Les inn alle tallene på første linje
+nums = [int(x) for x in input().split()]
+
+# Print ut summen av de to tallene i lista
+print(nums[0] + nums[1])
+
+# Sjekk at testfilene fungerer ved å kjøre med inputfilene:
+# python two_sum.py < two_sum_samples/sample1.in
+# python two_sum.py < two_sum_samples/sample2.in
